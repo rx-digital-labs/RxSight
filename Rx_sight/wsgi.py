09c857e1ce76c91d0bb0cb5fbcc8054f7b4528a8
MayaMalkoti <mayank.malkoti@rxdigital.co.in>
2019-12-18 13:11:21 +0530

    My First ommit

---
+++ Rx_sight/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Rx_sight project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rx_sight.settings')
+
+application = get_wsgi_application()
---
+++ ranger/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RangerConfig(AppConfig):
+    name = 'ranger'
