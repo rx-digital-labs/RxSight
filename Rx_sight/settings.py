9c54d68d087094f2d009698dd76316b32da322a5
MayaMalkoti <mayank.malkoti@rxdigital.co.in>
2019-12-18 16:48:41 +0530

    applying static content in website

--- Rx_sight/settings.py
+++ Rx_sight/settings.py
@@ -124,4 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- ranger/views.py
+++ ranger/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.db.models import Count, Sum
+from django.db.models.query import RawQuerySet
+from django.db import connection
 
 # Create your views here.
+
+def index(request):
+    return render(request,'ranger/index.html')
